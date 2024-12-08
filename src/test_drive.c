537069610e369590b63efbe31e2f889d8fa55874
Kirill-Kobyakov <kkobyak2@binghamton.edu>
2024-12-07 23:19:11 -0500

    Update test_drive.c
    jittery but works, (turn is a little off)

--- src/test_drive.c
+++ src/test_drive.c
@@ -35,7 +35,13 @@
 #define ENC_R                   (1<<11)
 #define ENC                     (ENC_L | ENC_R)
 
-#define ENC_R_TO_INCH 45
+#define ENC_R_TO_INCH 50
+#define ENC_TO_DEGREES 2.8
+#define PWM_TOP 100
+
+void sleep_us(uint32_t delay) {
+    for(int i = 0; i < delay; i++);
+}
 
 void setDirectionForward() {
     set_gpio(L_MOTOR_DIRECTION_1);
@@ -65,6 +71,63 @@ void setDirectionLeft() {
     clear_gpio(R_MOTOR_DIRECTION_2);
 }
 
+void drive(uint32_t distance, uint8_t speed, float left_motor_bias) {
+	uint32_t pins_read;
+    uint32_t enc_l_cnt = 0;
+    uint32_t enc_r_cnt = 0;
+
+    uint32_t enc_l_lv = 0;
+    uint32_t enc_r_lv = 0;
+
+    unsigned int pwm_counter = 0;
+    uint8_t duty_l = speed;
+    uint8_t duty_r = speed;
+
+    do {
+        pins_read = read_gpio();
+        if(((pins_read & ENC_L) >> 10) && (enc_l_lv == 0))
+            enc_l_cnt++;
+        if(((pins_read & ENC_R) >> 11) && (enc_r_lv == 0))
+            enc_r_cnt++;
+
+        enc_l_lv = (pins_read & ENC_L) >> 10;
+        enc_r_lv = (pins_read & ENC_R) >> 11;
+
+        pwm_counter++;
+        if (pwm_counter >= PWM_TOP)
+            pwm_counter = 0;
+
+        if (pwm_counter < duty_l)
+            set_gpio(PWM_L);
+        else
+            clear_gpio(PWM_L);
+
+        if (pwm_counter < duty_r)
+            set_gpio(PWM_R);
+        else
+            clear_gpio(PWM_R);
+
+        if(enc_r_cnt < enc_l_cnt * left_motor_bias){
+            if(duty_r + 3 <= PWM_TOP)
+                duty_r += 3;
+            if(duty_l - 3 >= 0)
+                duty_l -= 3;
+        }
+
+        if(enc_l_cnt * left_motor_bias < enc_r_cnt){
+            if(duty_l + 3 <= PWM_TOP)
+                duty_l += 3;
+            if(duty_r - 3 >= 0)
+                duty_r -= 3;
+        }
+        sleep_us(10);
+    } while (enc_r_cnt < distance);
+
+    clear_gpio(PWM_L);
+    clear_gpio(PWM_R);
+
+}
+
 
 int main ( void )
 {
@@ -74,37 +137,35 @@ int main ( void )
     clear_gpio_output_enable(ENC);
 
 	uint32_t pins_read;
-
-    // Quad Encoder Variables
     target:
-    uint32_t lm_quad_count = 0, rm_quad_count = 0;
-    uint8_t lm_quad_lv = 0;
-    uint8_t rm_quad_lv = 0; 
 
-    // PWM variables
-    uint16_t pwm_top;
-    uint16_t l_pwm_thresh;
-    uint16_t r_pwm_thresh;
+    while((read_gpio() & BTNR) == 0);
 
-    // Drive variables
-    uint16_t count;
+    setDirectionForward();
+    drive(30 * ENC_R_TO_INCH, 80, 1);
+
+    sleep_us(10000);
 
     setDirectionRight();
+    //drive(100 * ENC_R_TO_INCH, 80, 1);
+    drive(90 * ENC_TO_DEGREES, 80, 1);
+    
+    sleep_us(10000);
+
+
+    setDirectionForward();
+    drive(30 * ENC_R_TO_INCH, 80, 1);
+    // setDirectionRight();
+    // drive(90 * 2, 80, 0.2);
 
 	while(1){
 		pins_read = read_gpio();
-        if(((pins_read & ENC_L) >> 10) && (lm_quad_lv == 0))
-            lm_quad_count++;
-        if(((pins_read & ENC_R) >> 11) && (rm_quad_lv == 0))
-            rm_quad_count++;
 
-        lm_quad_lv = ((pins_read & ENC_L) >> 10);
-        rm_quad_lv = ((pins_read & ENC_R) >> 11);
 
-        if((rm_quad_count / ENC_R_TO_INCH) % 2 == 0)
-            set_gpio(LED_RED);
-        else
-            clear_gpio(LED_RED);
+        // if((rm_quad_count / ENC_R_TO_INCH) % 2 == 0)
+        //     set_gpio(LED_RED);
+        // else
+        //     clear_gpio(LED_RED);
 
         if(pins_read & ENC_L)
             set_gpio(LED_GRN);
@@ -120,13 +181,6 @@ int main ( void )
         else
             clear_gpio(LED_BLU);
 
-        if (rm_quad_count < ENC_R_TO_INCH * 10) {
-            set_gpio(PWM_L);
-            set_gpio(PWM_R);
-        } else {
-            clear_gpio(PWM_L);
-            clear_gpio(PWM_R);
-        }
         if(pins_read & BTNR)
             goto target;
 
